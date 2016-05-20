b1944ed594e23b0172405bacd3846a2099eb4bf7
Andrew Yip <yipcma@gmail.com>
2016-05-20 16:55:52 +0300

    component properties

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Comment from './Comment'
 
 const CommentList = React.createClass({
   render() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="John">This is one comment</Comment>
+        <Comment author="Mary"> This is comment number two. </Comment>
       </div>
     )
   }
