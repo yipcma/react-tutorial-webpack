b36b15c44913a7143021239c59962788846647ac
Andrew Yip <yipcma@gmail.com>
2016-05-20 14:59:58 +0300

    your first component: CommentBox

---
+++ src/components/CommentBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CommentBox = React.createClass({
+  render() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    )
+  }
+})
+
+export default CommentBox
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './components/CommentBox'
+
+// Render the main component into the dom
+ReactDOM.render(<CommentBox />, document.getElementById('app'));
