a5678903ade47ba8318dad80b6d089fa51978ec7
Andrew Yip <yipcma@gmail.com>
2016-05-21 03:12:04 +0300

    controlled components

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,15 +1,26 @@
 import React from 'react'
 
 const CommentForm = React.createClass({
+  getInitialState() {
+    return {author: '', text: ''}
+  },
+
+  // this uses events, could have used refs, and no need for state in form
+  handleAuthorChange(e) {
+    this.setState({author: e.target.value})
+  },
+
+  handleTextChange(e) {
+    this.setState({text: e.target.value})
+  },
+
   render() {
     return (
-      <div className="commentForm">
-        <form className="commentForm">
-          <input type="text" placeholder="Your name" />
-          <input type="text" placeholder="Say something..." />
-          <input type="submit" value="Post" />
-        </form>
-      </div>
+      <form className="commentForm">
+        <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+        <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
+        <input type="submit" value="Post" />
+      </form>
     )
   }
 })
