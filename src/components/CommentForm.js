9d52cf9de8636042e100cd4e86c5a08852fdd425
Andrew Yip <yipcma@gmail.com>
2016-05-21 03:18:28 +0300

    handleSubmit in form

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -14,9 +14,21 @@ const CommentForm = React.createClass({
     this.setState({text: e.target.value})
   },
 
+  handleSubmit(e) {
+    e.preventDefault()
+    const author = this.state.author.trim()
+    const text = this.state.text.trim()
+    if (!text || !author) {
+      return
+    }
+    // TODO send request to server
+    // if not using state, could just do form.reset()
+    this.setState({author: '', text: ''})
+  },
+
   render() {
     return (
-      <form className="commentForm">
+      <form className="commentForm" onSubmit={this.handleSubmit}>
         <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
         <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
         <input type="submit" value="Post" />
